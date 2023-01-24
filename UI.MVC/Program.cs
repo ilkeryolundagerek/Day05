f83c76a4c05f448362b3bf6d4bf7ee8a045dbe0b
ilker turan <ilkerturan@outlook.com>
2023-01-24 13:29:40 +0300

    24.01.2023 13:30

--- UI.MVC/Program.cs
+++ UI.MVC/Program.cs
@@ -3,9 +3,34 @@ using Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder
+    .Services
+    .AddControllersWithViews()
+    .AddRazorRuntimeCompilation()
+    .AddMvcLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+//Multilanguage:
+builder.Services.Configure<RequestLocalizationOptions>(opt =>
+{
+    var supportedCultures = new[] { "tr-TR", "en-US", "es-ES" };
+    opt.SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+});
+
 builder.Services.AddBaseServices();
+
 var app = builder.Build();
+
+//Multilanguage:
+var supportedCultures = new[] { "tr-TR", "en-US", "es-ES" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+app.UseRequestLocalization(localizationOptions);
 
 //app.AddAccessKeyCheckMiddlewares();
 app.AddExceptionCenter();
